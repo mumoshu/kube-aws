760af7e3672bb6fac15a3425ae1791fdfbe8d68f
Yusuke Kuoka <ykuoka@gmail.com>
2017-08-23 14:18:27 +0900

    Better documentation in the publishing script

--- ci/publish-docs-as-bot.sh
+++ ci/publish-docs-as-bot.sh
@@ -2,7 +2,10 @@
 
 set -ve
 
-# Requires the following command beforehand:
+# This script assumes you've created a ssh key used by kube-aws-bot to ssh into github:
+#   https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
+
+# It requires the following command beforehand:
 #   $ gem install travis
 #   $ travis login --auto
 #   $ travis encrypt-file ci/kube-aws-bot-git-ssh-key --repo <your github user or organization>/kube-aws
@@ -10,13 +13,13 @@ set -ve
 # And then change this line to the one output from the `travis encrypt-file` command above
 openssl aes-256-cbc -K $encrypted_514cf8442810_key -iv $encrypted_514cf8442810_iv -in kube-aws-bot-git-ssh-key.enc -out ci/kube-aws-bot-git-ssh-key -d
 
-# Prevent the following error:
-#   Permissions 0644 for '/home/travis/gopath/src/github.com/kubernetes-incubator/kube-aws/ci/kube-aws-bot-git-ssh-key' are too open.
-#   ...
-#   bad permissions: ignore key: /home/travis/gopath/src/github.com/kubernetes-incubator/kube-aws/ci/kube-aws-bot-git-ssh-key
+## Prevent the following error:
+##   Permissions 0644 for '/home/travis/gopath/src/github.com/kubernetes-incubator/kube-aws/ci/kube-aws-bot-git-ssh-key' are too open.
+##   ...
+##   bad permissions: ignore key: /home/travis/gopath/src/github.com/kubernetes-incubator/kube-aws/ci/kube-aws-bot-git-ssh-key
 chmod 600 ci/kube-aws-bot-git-ssh-key
 
-# Finally,
+# Finally run the following command to add the encrypted key to the git repo:
 #   $ git add kube-aws-bot-git-ssh-key.enc
 #   $   $ git commit -m '...'
 
