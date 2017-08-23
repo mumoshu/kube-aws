9e86d58356840bf7981e85f95d2db9469a35a287
Yusuke Kuoka <ykuoka@gmail.com>
2017-08-23 13:45:44 +0900

    Fix permission

--- ci/publish-docs-as-bot.sh
+++ ci/publish-docs-as-bot.sh
@@ -5,11 +5,16 @@ set -ve
 # Requires the following command beforehand:
 #   $ gem install travis
 #   $ travis login --auto
-#   $ chmod 600 ci/kube-aws-bot-git-ssh-key
 #   $ travis encrypt-file ci/kube-aws-bot-git-ssh-key --repo <your github user or organization>/kube-aws
 
 # And then change this line to the one output from the `travis encrypt-file` command above
 openssl aes-256-cbc -K $encrypted_514cf8442810_key -iv $encrypted_514cf8442810_iv -in kube-aws-bot-git-ssh-key.enc -out ci/kube-aws-bot-git-ssh-key -d
+
+# Prevent the following error:
+#   Permissions 0644 for '/home/travis/gopath/src/github.com/kubernetes-incubator/kube-aws/ci/kube-aws-bot-git-ssh-key' are too open.
+#   ...
+#   bad permissions: ignore key: /home/travis/gopath/src/github.com/kubernetes-incubator/kube-aws/ci/kube-aws-bot-git-ssh-key
+chmod 600 ci/kube-aws-bot-git-ssh-key
 
 # Finally,
 #   $ git add kube-aws-bot-git-ssh-key.enc
