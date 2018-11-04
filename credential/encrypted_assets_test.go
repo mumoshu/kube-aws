1f7e53958d07d2745739b5eff684343d4e6ad001
Yusuke KUOKA <ykuoka@gmail.com>
2018-11-04 13:54:06 +0900

    fix test/import cycle

--- credential/encrypted_assets_test.go
+++ credential/encrypted_assets_test.go
@@ -3,10 +3,7 @@ package credential
 import (
 	"testing"
 
-	"crypto/rsa"
-	"crypto/x509"
 	"encoding/base64"
-	"encoding/pem"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -14,26 +11,8 @@ import (
 
 	"fmt"
 	"github.com/aws/aws-sdk-go/service/kms"
-	"github.com/kubernetes-incubator/kube-aws/pkg/cluster"
-	"github.com/kubernetes-incubator/kube-aws/pkg/clusterapi"
-	"github.com/kubernetes-incubator/kube-aws/pki"
 	"github.com/kubernetes-incubator/kube-aws/test/helper"
 )
-
-const externalDNSNameConfig = `externalDNSName: test.staging.core-os.net
-`
-
-const availabilityZoneConfig = `availabilityZone: us-west-1c
-`
-
-const apiEndpointMinimalConfigYaml = `keyName: test-key-name
-region: us-west-1
-s3URI: s3://mybucket/mydir
-clusterName: test-cluster-name
-kmsKeyArn: "arn:aws:kms:us-west-1:xxxxxxxxx:key/xxxxxxxxxxxxxxxxxxx"
-`
-const minimalConfigYaml = externalDNSNameConfig + apiEndpointMinimalConfigYaml
-const singleAzConfigYaml = minimalConfigYaml + availabilityZoneConfig
 
 var goodNetworkingConfigs = []string{
 	``, //Tests validity of default network config values
@@ -77,118 +56,6 @@ func (d *dummyEncryptService) Encrypt(input *kms.EncryptInput) (*kms.EncryptOutp
 	}
 	numEncryption += 1
 	return &output, nil
-}
-
-func genAssets(t *testing.T) *RawAssetsOnMemory {
-	c, err := cluster.ClusterFromBytes([]byte(singleAzConfigYaml))
-	if err != nil {
-		t.Fatalf("failed generating config: %v", err)
-	}
-
-	caKey, caCert, err := pki.NewCA(c.TLSCADurationDays)
-	if err != nil {
-		t.Fatalf("failed generating tls ca: %v", err)
-	}
-	cfg, err := cluster.Compile(c, clusterapi.ClusterOptions{})
-	r := cluster.NewCredentialRenderer(cfg)
-	assets, err := r.NewAssetsOnMemory(caKey, caCert, true)
-	if err != nil {
-		t.Fatalf("failed generating assets: %v", err)
-	}
-
-	return assets
-}
-
-func TestTLSGeneration(t *testing.T) {
-	assets := genAssets(t)
-
-	pairs := []*struct {
-		Name      string
-		KeyBytes  []byte
-		CertBytes []byte
-		Key       *rsa.PrivateKey
-		Cert      *x509.Certificate
-	}{
-		//CA MUST come first
-		{
-			Name:      "ca",
-			KeyBytes:  assets.CAKey,
-			CertBytes: assets.CACert,
-		},
-		{
-			Name:      "apiserver",
-			KeyBytes:  assets.APIServerKey,
-			CertBytes: assets.APIServerCert,
-		},
-		{
-			Name:      "kube-controller-manager",
-			KeyBytes:  assets.KubeControllerManagerKey,
-			CertBytes: assets.KubeControllerManagerCert,
-		},
-		{
-			Name:      "kube-scheduler",
-			KeyBytes:  assets.KubeSchedulerKey,
-			CertBytes: assets.KubeSchedulerCert,
-		},
-		{
-			Name:      "apiserver-aggregator",
-			KeyBytes:  assets.APIServerAggregatorKey,
-			CertBytes: assets.APIServerAggregatorCert,
-		},
-		{
-			Name:      "admin",
-			KeyBytes:  assets.AdminKey,
-			CertBytes: assets.AdminCert,
-		},
-		{
-			Name:      "worker",
-			KeyBytes:  assets.WorkerKey,
-			CertBytes: assets.WorkerCert,
-		},
-		{
-			Name:      "etcd",
-			KeyBytes:  assets.EtcdKey,
-			CertBytes: assets.EtcdCert,
-		},
-	}
-
-	var err error
-	for _, pair := range pairs {
-
-		if keyBlock, _ := pem.Decode(pair.KeyBytes); keyBlock == nil {
-			t.Errorf("Failed decoding pem block from %s", pair.Name)
-		} else {
-			pair.Key, err = x509.ParsePKCS1PrivateKey(keyBlock.Bytes)
-			if err != nil {
-				t.Errorf("Failed to parse key %s : %v", pair.Name, err)
-			}
-		}
-
-		if certBlock, _ := pem.Decode(pair.CertBytes); certBlock == nil {
-			t.Errorf("Failed decoding pem block from %s", pair.Name)
-		} else {
-			pair.Cert, err = x509.ParseCertificate(certBlock.Bytes)
-			if err != nil {
-				t.Errorf("Failed to parse cert %s: %v", pair.Name, err)
-			}
-		}
-	}
-
-	t.Log("Assets assets parsed successfully")
-
-	if t.Failed() {
-		t.Fatalf("Assets key pairs not parsed, cannot verify signatures")
-	}
-
-	caCert := pairs[0].Cert
-	for _, pair := range pairs[1:] {
-		if err := pair.Cert.CheckSignatureFrom(caCert); err != nil {
-			t.Errorf(
-				"Could not verify ca certificate signature %s : %v",
-				pair.Name,
-				err)
-		}
-	}
 }
 
 func TestReadOrCreateCompactAssets(t *testing.T) {
